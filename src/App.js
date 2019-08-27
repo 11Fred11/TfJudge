92e036e12240f03998786ec38478c463f59f17e7
10Fred10 <contact.hammami.fredj@gmail.com>
2019-08-27 13:49:50 +0100

    fetches multiple users

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import React from "react";
-import "./App.css";
-import Player from "./Player/Player";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>HELLO THERE !</h1>
-      <Player />
-    </div>
-  );
+class App extends Component {
+  state = {
+    inputs: ["fredjie", "salipow"],
+    loading: true,
+    players: []
+  };
+  componentDidMount() {
+    let joined = [];
+    this.state.inputs.map(name => {
+      fetch(
+        `https://cors-anywhere.herokuapp.com/https://api.tracker.gg/api/v2/tft/standard/profile/riot/${name}?region=EUW`
+      )
+        .then(res => res.json())
+        .then(data => {
+          joined = [...this.state.players];
+          joined.push(data);
+          this.setState({
+            inputs: ["fredjie", "salipow"],
+            loading: false,
+            players: joined
+          });
+        })
+        .catch(console.log);
+    });
+  }
+
+  render() {
+    //console.log(this.state.players);
+    return (
+      <div className="container">
+        <div className="col-xs-12">
+          <h1>PLAYERS</h1>
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">
+                {this.state.loading && <p>Loading...</p>}
+                {!this.state.loading &&
+                  this.state.players[0].data.platformInfo
+                    .platformUserIdentifier}
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
